12765784091f4976f54070f89d48996d185ffc5a
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-09-14 14:43:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header'
+function App() {
+  
+  
+  return (
+    <>
+      <div className='flex justify-start'>
+        <div>
+          <Header/>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/coffee-logo.jpg'
+
+const header = () => {
+  return (
+    <div>
+        <h1 className='text-8xl font-bold'>Coco.</h1>
+        <img className='w-24 h-24' src="https://c8.alamy.com/comp/2NPD1GX/minimalist-black-and-white-cup-of-tea-or-coffee-with-steam-vector-illustration-2NPD1GX.jpg" alt="" />
+        <h5>Read all your tech news (TechCrunch, Hacker News, Product Hunt and many more) in one place.</h5>
+    </div>
+  )
+}
+
+export default header
