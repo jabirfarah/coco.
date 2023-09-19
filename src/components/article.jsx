c815b21a4f700e8a4a8e671fe66f8a6503305529
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-09-19 18:56:09 +0100

    Added time from article

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,50 +1,84 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 // This gives the top stories
 // https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
 
 
-// Need to get 30 ID's from the top stories API Call
+// Need to get 30 IDs from the top stories API Call
 // Then take that ID and run the story API call on all of them
 
-
-
-
 const Article = () => {
-        const [hn, setHN] = useState([
-
-        ])
-        const comsec = async () => {
+        const [hn, setHN] = useState([])
+        const cos = async () => {
+          //Get top stories from Hacker News (only gives me ID's)
             const topStoriesHN = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
             const res = await fetch(topStoriesHN)
             const json = await res.json()
             const hnStringify = await JSON.parse(JSON.stringify(json))
-
-            const lst = []
-            for (let i = 0; i <= 29 ; i++) {
-                lst.push(hnStringify[i]);
-             
-                
-            }
             //push Hacker News stories to state
             const lstStory = []
-            for (let index = 0; index <= lst.length; index++) {
+            // Only getting 30 items as that is the amount for the front page.
+            for (let index = 0; index <= 29; index++) {
+                // Take ID's and fetch the stories
                 const storyHN = await fetch(`https://hacker-news.firebaseio.com/v0/item/${hnStringify[index]}.json?print=pretty`)
                 const storyRes = await storyHN.json()
-                lstStory.push(storyRes)
+                lstStory.push(storyRes);
             }
+
+
+
             setHN(lstStory)
     }       
- comsec();
- let index = 0
+ cos().then(() => {
+
+ })
+
+
+// const getDate = (d) => {
+//   return new Date(d * 1000)
+// }
+
+const getReadableDate = (d) => {
+  const date = new Date(d * 1000)
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  month = month.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  let day = date.getDate();
+  day = day.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  let hour = date.getHours()
+  hour = hour.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+  let minutes = date.getMinutes();
+  minutes = minutes.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+
+  const m = date.getMinutes();
+  const currDate = new Date();
+
+  var hours = (currDate.getTime() - date.getTime()) / ( 1000 * 60 * 60);
+  var mins = (currDate.getTime() - date.getTime()) / ( 1000 * 60);
+  if  (hours < 1) {
+      hours = 0;
+  }
+
+
+  // const h = Math.floor(seconds / 3600);
+  // const min = Math.floor(seconds / 60) % 60;
+  // seconds = Math.floor(seconds);
+
+
+    return `${hours > 0 ? `${Math.round(hours)} hours ago` : `${Math.round(mins)} minutes ago`}`;
+}
+
   return (
-    hn.map((story) => (
-        
-        <div key={index + 1}>
-            <a key={index++} href={story.url} target='_blank'>{story.title}</a>
-            </div>
-        
-    ))
+
+      <ul>
+      { hn.map((article) => (
+            <li key={article.id}>
+                <a rel="noreferrer" href={article.url} target="_blank">{article.title}</a>
+                <div className="text-orange-600">Hacker News</div>
+                <div>{getReadableDate(article.time)}</div>
+            </li>
+      ))}
+      </ul>
   )
 }
 
